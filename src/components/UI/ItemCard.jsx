7ec7e2ac3117dd5187668d10f73973d6b89ae267
Alex Nelson <nelsona3017@gmail.com>
2025-12-01 23:29:21 -0500

    feat: updated Link paths in itemcard component for dynamic routing

--- src/components/UI/ItemCard.jsx
+++ src/components/UI/ItemCard.jsx
@@ -67,7 +67,7 @@ export default function ItemCard({ item, loading, explore }) {
       <div className="nft__item">
         <div className="author_list_pp">
           <Link
-            to="/author"
+            to={`/author/${item.authorId}`}
             data-bs-toggle="tooltip"
             data-bs-placement="top"
             title={item?.title}
@@ -98,7 +98,7 @@ export default function ItemCard({ item, loading, explore }) {
             </div>
           </div>
 
-          <Link to="/item-details">
+          <Link to={`/item-details/${item.nftId}`}>
             <img
               src={item?.nftImage}
               className="lazy nft__item_preview"
@@ -107,7 +107,7 @@ export default function ItemCard({ item, loading, explore }) {
           </Link>
         </div>
         <div className="nft__item_info">
-          <Link to="/item-details">
+          <Link to={`/item-details/${item.nftId}`}>
             <h4>{item?.title}</h4>
           </Link>
           <div className="nft__item_price">{item?.price} ETH</div>
